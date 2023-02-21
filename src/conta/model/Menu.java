39c09d73e9724cc8a891f8f46dd939adba64050c
Fewatts <feralveswatts@gmail.com>
2023-02-21 17:49:42 -0300

    Login e criar conta inseridos no main(Mas falta login dinâmico)

--- src/conta/model/Menu.java
+++ src/conta/model/Menu.java
@@ -1,58 +1,83 @@
 package conta.model;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import conta.util.Conta;
 
 public class Menu {
+	
+	static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-	Scanner leia = new Scanner(System.in);
-	String usuario, senha;
-	int opcao;
-	while(true) {
-		System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
-		System.out.println("                                                                   ");
-		System.out.println("                     BEM-VINDE À GENFLIX                           ");
-		System.out.println("                                                                   ");
-		System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
-		System.out.println("                                                                   ");
-		System.out.println("                   1- Fazer login                                  ");
-		System.out.println("                   2- Criar uma conta                              ");
-		System.out.println("                   3- Encerrar                                     ");
-		System.out.println("                                                                   ");
-		System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
-		System.out.println("¬¬¬Digite sua opção:                                               ");
-		System.out.println("                                                                   ");
-		opcao = leia.nextInt();
-		if(opcao == 3) {
-			System.out.println("Sistema encerrado com sucesso!");
-			System.out.println("Volte sempre!\n\n");
-			leia.close();
-			System.exit(0);
-		}
-		switch(opcao){
-		case 1 ->{
-			System.out.println("Fazer login\n\n");
-			System.out.println("Usuário: ");
-			leia.skip("\\R?");
-			usuario = leia.nextLine();
-			
-			System.out.println("Senha: ");
-			leia.skip("\\R?");
-			senha = leia.nextLine();
-			if(Conta.verificaLogin(usuario, senha)) {
-				System.out.println("Logado!\n\n");
-				//menuUsuario();
+		ArrayList<Conta> contas = new ArrayList<Conta>();
+		Conta c_1 = new Conta("teste", "123");
+		contas.add(c_1);
+		
+		String usuario, senha;
+		int opcao, resp;
+		
+		while(true) {
+			System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+			System.out.println("                                                                   ");
+			System.out.println("                     BEM-VINDE À GENFLIX                           ");
+			System.out.println("                                                                   ");
+			System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+			System.out.println("                                                                   ");
+			System.out.println("                   1- Fazer login                                  ");
+			System.out.println("                   2- Criar uma conta                              ");
+			System.out.println("                   3- Encerrar                                     ");
+			System.out.println("                                                                   ");
+			System.out.println("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+			System.out.println("¬¬¬Digite sua opção:                                               ");
+			System.out.println("                                                                   ");
+			opcao = leia.nextInt();
+			if(opcao == 3) {
+				System.out.println("Sistema encerrado com sucesso!");
+				System.out.println("Volte sempre!\n\n");
+				System.exit(0);
 			}
-		}
-		case 2 ->{
-			System.out.println("Criar conta:\n\n");
-			Conta.criarConta();
-			//menuUsuario();
+			switch(opcao){
+			case 1 ->{
+				System.out.println("Fazer login\n\n");
+				System.out.println("Usuário: ");
+				leia.skip("\\R?");
+				usuario = leia.nextLine();
+				
+				System.out.println("Senha: ");
+				leia.skip("\\R?");
+				senha = leia.nextLine();
+				
+				if(usuario.equals(contas.get(0).getUsuario()) && senha.equals(contas.get(0).getSenha())) { 
+					System.out.println("Logado! \n");
+					//usar contas.get(0)
+					System.out.println("Menu usuário:\n\n");
+				}
+				}
+			case 2 ->{
+				System.out.println("Criar conta:\n\n");
+				while(true) {
+					System.out.println("Usuário: ");
+					leia.skip("\\R?");
+					usuario = leia.nextLine();
+					
+					System.out.println("Senha: ");
+					leia.skip("\\R?");
+					senha = leia.nextLine();
+					
+					System.out.printf("\nUsuário: %s\nSenha: %s\nConfirma os dados? [1- sim/2-não]\n", usuario, senha);
+					resp = leia.nextInt();
+					if(resp == 1) {
+						Conta c_2 = new Conta(usuario, senha);
+						contas.add(c_2);
+						System.out.printf("Conta %s criada com sucesso!\n\n", c_2.getUsuario());
+						break;
+					}
 			
-		}
-		default ->System.out.println("Opção inválida, tente novamente...");
-		}
+				}
+				
+			}
+			default ->System.out.println("Opção inválida, tente novamente...");
+		 }
 		
 	  }
 	
--- src/conta/util/Conta.java
+++ src/conta/util/Conta.java
@@ -1,8 +1,5 @@
 package conta.util;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Conta {
 	private String usuario;
 	private String senha;
@@ -21,39 +18,5 @@ public class Conta {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}
-	public static void criarConta() {
-		String usuario, senha;
-		int resp;
-		Scanner leia = new Scanner(System.in);
-		ArrayList<Conta> contas = new ArrayList<Conta>();
-		while(true) {
-			System.out.println("Usuário: ");
-			leia.skip("\\R?");
-			usuario = leia.nextLine();
-			
-			System.out.println("Senha: ");
-			leia.skip("\\R?");
-			senha = leia.nextLine();
-			
-			System.out.printf("\nUsuário: %s\nSenha: %s\nConfirma os dados? [1- sim/2-não]\n", usuario, senha);
-			resp = leia.nextInt();
-			if(resp == 1) {
-				Conta c_1 = new Conta(usuario, senha);
-				contas.add(c_1);
-				System.out.printf("Conta %s criada com sucesso!\n\n", c_1.getUsuario());
-				break;
-			}
-	
-		}
-		
-	}
-	public static boolean verificaLogin(String user, String senha){
-		Conta c_2 = new Conta("teste", "123");
-		if(user.equals(c_2.usuario) && senha.equals(c_2.senha)) 
-			return true;
-		else
-			return false;			
-	}
-	
+	}	
 }
